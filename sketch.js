e73b78d7a1bb92fa7053703216ac261364c29c9f
mythreyan07 <77281857+mythreyan07@users.noreply.github.com>
2022-01-30 10:16:03 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -13,15 +13,15 @@ var cars, car1, car2, car3, car4;
 var track, car1_img, car2_img, car3_img, car4_img;
 
 function preload(){
-  track = loadImage("../images/track.jpg");
-  car1_img = loadImage("../images/car1.png");
-  car2_img = loadImage("../images/car2.png");
-  car3_img = loadImage("../images/car3.png");
-  car4_img = loadImage("../images/car4.png");
-  ground = loadImage("../images/ground.png");
-  bronze_img = loadImage("images/bronze.png");
-  silver_img = loadImage("images/silver.png");
-  gold_img = loadImage("images/gold.png");
+  track = loadImage("track.jpg");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  car3_img = loadImage("car3.png");
+  car4_img = loadImage("car4.png");
+  ground = loadImage("ground.png");
+  bronze_img = loadImage("bronze.png");
+  silver_img = loadImage("silver.png");
+  gold_img = loadImage("gold.png");
 }
 
 function setup(){
